657c0e99d68a8b69fc0a18a50f68969f99c4c5bc
hgq1211 <717042959@qq.com>
2016-08-03 20:41:28 +0800

    angular 中文网 demo

---
+++ app/main.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2016/8/3.
+ */
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import {AppComponent} from './app.component';
+bootstrap(AppComponent);
